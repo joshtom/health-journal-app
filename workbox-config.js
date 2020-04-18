6e5ccfc17ddee508453fc8e8caddcd3a461baab6
joshtom <olajidejoshua4real@gmail.com>
2020-04-18 15:16:16 +0100

    Added express server, workbox and sass

---
+++ workbox-config.js
@@ -0,0 +1,28 @@
+/** Cache all the files in the build directory */
+// module.exports = {
+//   "globDirectory": "build/",
+//   "globPatterns": [
+//     "**/*.{jpg,svg,html,js,css}"
+//   ],
+//   "swDest": "build/sw.js",
+//   "swSrc": "src/sw.js"
+// };
+
+// Caching only the files in homepage to make it only available when offline 
+module.exports = {
+  "globDirectory": "build/",
+  "globPatterns": [
+    "**/*.css",
+    "**/*.scss",
+    "js/animation.js",
+    "index.html",
+    "manifest.webmanifest",
+    "images/*.png",
+    "images/.svg",
+  ],
+  "swSrc": "src/sw.js",
+  "swDest": "build/sw.js",
+  "globIgnores": [
+    "../workbox-config.js"
+  ]
+};
