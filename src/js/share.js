c83539a6e01054e17d7ec0001c76a890ec6a05a9
joshtom <olajidejoshua4real@gmail.com>
2020-04-30 19:08:38 +0100

    Fixed conditional statement not executing

--- src/js/share.js
+++ src/js/share.js
@@ -1,4 +1,33 @@
-document.querySelector("#shareNow").addEventListener('click', (e) => {
-    e.preventDefault();
-    alert('Are you working?');
-  })
+
+
+  const getStorage = (key = 'key') => {
+    return localStorage.getItem(key);
+  }
+
+  const getHowdy = getStorage('howdyValue');
+  const gettoday = getStorage('todayValue');
+  const getelaborate = getStorage('elaborateValue');
+  const gettellUs = getStorage('tellUsValue');
+  const getfeel = getStorage('feelValue');
+
+ 
+
+  subject.innerHTML = `${getHowdy} ${gettoday} ${getelaborate} ${gettellUs} ${getfeel}`;
+
+        const btn = document.querySelector("#shareNow");
+        btn.addEventListener('click', (e) => {
+            e.preventDefault();
+            const name = document.querySelector("#Name").value;
+            const email = document.querySelector("#Email").value;
+            const subject = document.querySelector("textarea").value;
+            if(name === ''|| email === ''|| subject === '') {
+                alert("Field empty");
+            } else if(name !== '' || email !== '' || subject !== ''){
+            alert("Your health status has been shared");
+
+            return setTimeout(() => {
+                window.location = '/';
+            }, 1500)
+          }
+            })
+        
